02040c4e557e470dbdba804e3a4992fb768434af
liuqi15 <liuqi15@baidu.com>
2018-06-10 18:04:41 +0800

    gdb watch

---
+++ gdb/watch/set-watchpoint.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <unistd.h>
+int a = 0;
+
+void *thread1_func(void *p_arg)
+{
+    while (1)
+    {
+            a++;
+            sleep(10);
+    }
+}
+
+int main(int argc, char* argv[])
+{
+    pthread_t t1;
+    pthread_create(&t1, NULL, thread1_func, NULL);
+
+    sleep(1000);
+    return 0;
+}
